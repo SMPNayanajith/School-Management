1ee3d7da4cdcf9eb8da3bcbede2e3f5b746684bd
SMPNayanajith <pasindusamarakoon500@gmail.com>
2024-03-03 22:18:43 +0530

    commit

--- src/component/Students/Students.jsx
+++ src/component/Students/Students.jsx
@@ -1,55 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Students.css';
 import { Link } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
+import axios from 'axios';
 
 export default function Students() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    loadStudents();
+  }, []);
+
+  const loadStudents = async () => {
+    const result = await axios.get('http://localhost:8080/api/v1/student/get');
+    setStudents(result.data);
+  };
+
   return (
     <div>
-      <NavBar/>
-      <button type="button"   class="btn btn-primary btn-lg buttonstyle">
-      <Link to="/CreateStudent" className='buttonlink'>Create Student</Link> 
-        </button>
+      <NavBar />
+      <button type="button" class="btn btn-primary btn-lg buttonstyle">
+        <Link to="/CreateStudent" className='buttonlink'>Create Student</Link>
+      </button>
 
-        <table class="table shadow  tabledata">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Full Name</th>
-      <th scope="col">Email </th>
-      <th scope="col">Registration No.</th>
-      <th scope="col">Address</th>
-      <th scope="col">Birthday</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-
+      <table class="table shadow tabledata">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Full Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Registration No.</th>
+            <th scope="col">Address</th>
+            <th scope="col">Birthday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{student.full_name}</td>
+              <td>{student.email_address}</td>
+              <td>{student.registration_no}</td>
+              <td>{student.address}</td>
+              <td>{student.birthday}</td>
+              <td>
+                <button class="btn btn-primary" type="submit">Update</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
--- src/component/Teacher/Teacher.jsx
+++ src/component/Teacher/Teacher.jsx
@@ -1,57 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import NavBar from '../NavBar/NavBar'
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
+import axios from 'axios';
 
 export default function Teacher() {
+  const [teachers, setTeachers] = useState([]);
+
+  useEffect(() => {
+    loadTeachers();
+  }, []);
+
+  const loadTeachers = async () => {
+    const result = await axios.get('http://localhost:8080/api/v1/teacher/get');
+    setTeachers(result.data);
+  };
+
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+      <button type="button" class="btn btn-primary btn-lg buttonstyle">
+        <Link to="/CreateTeacher" className='buttonlink'>Create Teacher</Link>
+      </button>
 
-       <button type="button" class="btn btn-primary btn-lg  buttonstyle">
-       <Link to="/CreateTeacher" className='buttonlink'>Create Teacher</Link>  
-        </button>
-
-        
-        <table class="table shadow  tabledata">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Full Name</th>
-      <th scope="col">Email </th>
-      <th scope="col">Registration No.</th>
-      <th scope="col">Address</th>
-      <th scope="col">Birthday</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        
+      <table class="table shadow tabledata">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Full Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Registration No.</th>
+            <th scope="col">Address</th>
+            <th scope="col">Birthday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teachers.map((teacher, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{teacher.full_name}</td>
+              <td>{teacher.email_address}</td>
+              <td>{teacher.registration_no}</td>
+              <td>{teacher.address}</td>
+              <td>{teacher.birthday}</td>
+              <td>
+                <button class="btn btn-primary" type="submit">Update</button>
+                <button type="button" class="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
